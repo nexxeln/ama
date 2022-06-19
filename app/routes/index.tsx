dd25a34ae66975cda22ee231dbd03caada445f7c
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-19 22:33:23 +0530

    create navbar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,32 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <main>
       <div className="mt-4" />
-      <h1 className="text-sky-200 text-2xl">nexxel • ama</h1>
+      <h1 className="text-sky-300 text-2xl text-center">nexxel • ama</h1>
+
+      <div className="mt-6" />
+      <nav>
+        <ul className="flex gap-6 justify-center text-sky-200">
+          <li>
+            <Link
+              to="/"
+              className="hover:text-sky-100 transition-colors duration-300"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/ask"
+              className="hover:text-sky-100 transition-colors duration-300"
+            >
+              Ask
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </main>
   );
 }
