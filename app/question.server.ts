96f3b6f12e1d268c22d6a17ea6ca27ba62f64377
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-20 16:24:58 +0530

    make createQuestion function typesafe

--- app/question.server.ts
+++ app/question.server.ts
@@ -1,6 +1,9 @@
+import { question } from "@prisma/client";
 import { prisma } from "./db.server";
 
-export const createQuestion = async (question) => {
+export const createQuestion = async (
+  question: Pick<question, "name" | "question">
+) => {
   await prisma.question.create({
     data: question,
   });
