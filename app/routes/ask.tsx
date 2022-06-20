39be02a401f17e747fdecbc65be2dc386f4d592f
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-20 17:08:32 +0530

    add loading state

--- app/routes/ask.tsx
+++ app/routes/ask.tsx
@@ -1,5 +1,5 @@
 import { ActionFunction, json } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { createQuestion } from "~/question.server";
 
@@ -40,6 +40,9 @@ const inputStyles =
 export default function Ask() {
   const errors = useActionData() as ActionData;
 
+  const transition = useTransition();
+  const loading = Boolean(transition.submission);
+
   return (
     <Form method="post">
       <main className="mt-8 text-sky-100">
@@ -75,9 +78,10 @@ export default function Ask() {
           <div className="mt-4" />
           <button
             type="submit"
-            className="flex items-center justify-center w-20 px-4 py-2 transition-all duration-300 rounded-md bg-sky-700 bg-opacity-30 hover:bg-opacity-75"
+            className="flex items-center justify-center w-24 px-4 py-2 transition-all duration-300 rounded-md bg-sky-700 bg-opacity-30 hover:bg-opacity-75 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:opacity-50"
+            disabled={loading}
           >
-            Submit
+            {loading ? "Loading..." : "Ask"}
           </button>
         </div>
       </main>
