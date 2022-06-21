df6419bbace8550dea07351c41f132811520922d
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-21 11:09:04 +0530

    create loader function to fetch data and make basic design

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,33 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getQuestions } from "~/question.server";
+
+type LoaderData = Awaited<ReturnType<typeof getQuestions>>;
+
+export const loader: LoaderFunction = async () => {
+  const questions = await getQuestions({ getAnswered: true });
+
+  return json(questions);
+};
+
 export default function Index() {
-  return <main className="text-white">home</main>;
+  const questions = useLoaderData() as LoaderData;
+
+  console.log(questions);
+
+  return (
+    <main className="w-3/4 mx-auto mt-8 md:w-2/5 text-sky-100">
+      {questions.map((question) => (
+        <>
+          <div className="flex items-baseline gap-2">
+            <p className="text-2xl text-sky-500">{question.question}</p>
+            <p className="text-sm">{`- ${question.name}`}</p>
+          </div>
+          <p>{question.answer}</p>
+          <div className="mb-4" />
+        </>
+      ))}
+    </main>
+  );
 }
