fb99a5d8e94e05b02716ffcb63c92e16c1fb03b2
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-21 11:33:10 +0530

    style changes and typesafety

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,7 +8,7 @@ type LoaderData = Awaited<ReturnType<typeof getQuestions>>;
 export const loader: LoaderFunction = async () => {
   const questions = await getQuestions({ getAnswered: true });
 
-  return json(questions);
+  return json<LoaderData>(questions);
 };
 
 export default function Index() {
@@ -18,15 +18,15 @@ export default function Index() {
 
   return (
     <main className="w-3/4 mx-auto mt-8 md:w-2/5 text-sky-100">
-      {questions.map((question) => (
-        <>
+      {questions.map((question, index) => (
+        <div key={index}>
           <div className="flex items-baseline gap-2">
             <p className="text-2xl text-sky-500">{question.question}</p>
-            <p className="text-sm">{`- ${question.name}`}</p>
+            <p className="text-sm text-sky-200">{`- ${question.name}`}</p>
           </div>
           <p>{question.answer}</p>
           <div className="mb-4" />
-        </>
+        </div>
       ))}
     </main>
   );
