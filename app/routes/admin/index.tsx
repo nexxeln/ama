5c9aff53657d6a510ffb3920d9f94fe38f4a62fa
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-21 12:07:16 +0530

    create admin route and form

---
+++ app/routes/admin/index.tsx
@@ -0,0 +1,23 @@
+import { Form } from "@remix-run/react";
+
+export default function Admin() {
+  return (
+    <Form method="post">
+      <div className="mt-6" />
+      <div className="flex flex-col text-sky-100">
+        <p>Enter password</p>
+        <div className="mb-1" />
+
+        <input type="text" placeholder="👀" className="input" />
+        <div className="mb-3" />
+
+        <button
+          type="submit"
+          className="flex items-center justify-center w-20 px-4 py-2 text-sm transition-all duration-300 rounded-md bg-sky-700 bg-opacity-30 hover:bg-opacity-75"
+        >
+          Submit
+        </button>
+      </div>
+    </Form>
+  );
+}
